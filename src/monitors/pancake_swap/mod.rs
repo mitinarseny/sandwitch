cad11581534624655695e99e3e1d565b1cb0e53d
Arseny Mitin <mitinarseny@gmail.com>
2022-08-29 21:41:05 +0300

    feat: collect data into Swap struct

--- src/monitors/pancake_swap/mod.rs
+++ src/monitors/pancake_swap/mod.rs
@@ -1,12 +1,12 @@
 use futures::future::BoxFuture;
-use futures::stream::{BoxStream, FuturesUnordered};
-use futures::{FutureExt, StreamExt, TryFutureExt, TryStreamExt};
+use futures::stream::BoxStream;
+use futures::{FutureExt, StreamExt, TryFutureExt};
 use serde::Deserialize;
 use std::collections::HashMap;
 use std::future;
 use web3::api::Eth;
 use web3::contract::tokens::Detokenize;
-use web3::types::{Address, Transaction, U256};
+use web3::types::{Address, Transaction, H256, U256};
 use web3::Transport;
 
 mod contracts;
@@ -64,7 +64,7 @@ impl<T: Transport> PancakeSwap<T> {
 impl<T: Transport> PancakeSwap<T> {
     fn check_swap_exact_eth_for_tokens(&self, tx: &Transaction) -> bool {
         !tx.value.is_zero()
-            && tx.value.bits() <= 128 // for U256::as_u128
+            && [tx.value, tx.gas].iter().all(|v| v.bits() <= 128) // for U256::as_u128
             && tx.to.map_or(false, |h| h == self.router.address())
             && tx.input.0.starts_with(
                 &contracts::SWAP_EXACT_ETH_FOR_TOKENS.short_signature(), // TODO:
@@ -94,6 +94,37 @@ impl<T: Transport> PancakeSwap<T> {
             .filter(|(_, path, ..)| path.len() == 2)
             .map(|(amount_out_min, path, ..)| (amount_out_min, (path[0], path[1])))
     }
+
+    async fn filter_map(&self, tx: Transaction) -> Option<Swap<'_, T>> {
+        if !self.check_swap_exact_eth_for_tokens(&tx) {
+            return None;
+        }
+
+        let (amount_out_min, (t0, t1)) = Self::decode_swap_exact_eth_for_tokens_input2(&tx)?;
+
+        let pair = self.pair_contracts.get(&(t0, t1))?;
+        let (t0, t1) = pair.tokens();
+
+        Some(Swap {
+            tx_hash: tx.hash,
+            gas: tx.gas.as_u128(),
+            gas_price: t0.as_decimals(tx.gas_price?.low_u128()),
+            amount_in: t0.as_decimals(tx.value.low_u128()),
+            amount_out_min: t1.as_decimals(amount_out_min),
+            reserves: pair.get_reserves().await.ok().map(|(r0, r1, _)| (r0, r1))?,
+            pair,
+        })
+    }
+}
+
+struct Swap<'a, T: Transport> {
+    tx_hash: H256,
+    gas: u128,
+    gas_price: f64,
+    amount_in: f64,
+    amount_out_min: f64,
+    reserves: (f64, f64),
+    pair: &'a Pair<T>,
 }
 
 impl<T> Monitor<Transaction> for PancakeSwap<T>
@@ -108,56 +139,34 @@ where
         stream: BoxStream<'a, Transaction>,
     ) -> BoxFuture<'a, Result<(), Self::Error>> {
         let mut stream = stream
-            .filter(|tx| future::ready(self.check_swap_exact_eth_for_tokens(tx)))
-            .filter_map(|tx| {
-                future::ready(
-                    Self::decode_swap_exact_eth_for_tokens_input2(&tx)
-                        .map(move |inputs| (tx, inputs)),
-                )
-            })
-            .filter_map(|(tx, (amount_out_min, (t0, t1)))| {
-                future::ready(
-                    self.pair_contracts
-                        .get(&(t0, t1))
-                        .map(|pair| (tx, amount_out_min, pair)),
-                )
-            })
-            .map(|(tx, amount_out_min, pair)| {
-                pair.get_reserves().map_ok(move |(r0, r1, _)| {
-                    let (t0, t1) = pair.tokens();
-                    let amount_in = t0.as_decimals(tx.value.low_u128());
-                    let gas = tx.gas.low_u128();
-                    let gas_price = tx.gas_price.map(|p| t0.as_decimals(p.low_u128()));
-                    let amount_out_min = t1.as_decimals(amount_out_min);
-                    (
-                        tx,
-                        gas,
-                        gas_price,
-                        amount_in,
-                        amount_out_min,
-                        (t0, t1),
-                        (r0, r1),
-                        pair,
-                    )
-                })
-            })
+            .map(|tx| self.filter_map(tx))
             .buffer_unordered(10)
-            .filter_map(|r| future::ready(r.ok()))
+            .filter_map(future::ready)
             .boxed();
 
         async move {
-            while let Some((tx, gas, gas_price, amount_in, amount_out_min, (t0, t1), (r0, r1), pair)) =
-                stream.next().await
-            {
-                dbg!("{:#x}", tx.hash);
+            while let Some(sw) = stream.next().await {
+                // dbg!("{:#x}", tx.hash);
+                let (t0, t1) = sw.pair.tokens();
                 println!(
-                    "{:#x}, {gas}, {}, {amount_in}, {amount_out_min}, {:#x}, {:#x}, {r0}, {r1}, {:#x}",
-                    tx.hash,
-                    gas_price.unwrap_or(0.0),
+                    "{:#x}, {}, {}, {}, {}, {:#x}, {:#x}, {}, {}, {:#x}, {:?}",
+                    sw.tx_hash,
+                    sw.gas,
+                    sw.gas_price,
+                    sw.amount_in,
+                    sw.amount_out_min,
                     t0.address(),
                     t1.address(),
-                    pair.address(),
-                    // calculate_max_amount(r0, r1, amount_in, amount_out_min),
+                    sw.reserves.0,
+                    sw.reserves.1,
+                    sw.pair.address(),
+                    calculate_amounts_in_and_out_min(
+                        sw.reserves.0,
+                        sw.reserves.1,
+                        sw.amount_in,
+                        sw.amount_out_min,
+                        3.0
+                    ),
                 );
             }
             Ok(())
@@ -168,8 +177,30 @@ where
 
 fn calculate_max_amount(pool_a: f64, pool_b: f64, his_value_a: f64, his_value_min_b: f64) -> f64 {
     0.5 * (((his_value_a * (4.0 * pool_a * pool_b + his_value_a * his_value_min_b))
-        / his_value_min_b
+        / his_value_min_b)
+        .sqrt()
         - 2.0 * pool_a
         - his_value_a)
-        .sqrt())
+}
+
+fn calculate_amounts_in_and_out_min(
+    pool_a: f64,
+    pool_b: f64,
+    his_value_a: f64,
+    his_value_min_b: f64,
+    our_limit_a: f64,
+) -> Option<(f64, f64)> {
+    let we_buy = calculate_max_amount(pool_a, pool_b, his_value_a, his_value_min_b);
+    if we_buy <= 0.0 {
+        return None;
+    }
+    let we_buy = we_buy.min(our_limit_a);
+    let we_get = (pool_b * we_buy) / (pool_a + we_buy);
+    let (g1, g2) = (0.0009, 0.0009); // TODO: gas price
+    let our_min_b =
+        (g1 + g2 + we_buy) * (pool_b - his_value_min_b) / (his_value_a + pool_a + we_buy);
+    if we_get <= our_min_b {
+        return None;
+    }
+    Some((we_buy, our_min_b))
 }
